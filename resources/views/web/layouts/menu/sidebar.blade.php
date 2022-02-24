68b0d8e34c11c269d68c41b64b5e1f26a8a23ebb
mkeremcansev <mkeremcansevmail.com>
2022-02-24 22:57:56 +0300

    category list front end updated

--- resources/views/web/layouts/menu/sidebar.blade.php
+++ resources/views/web/layouts/menu/sidebar.blade.php
@@ -1,10 +1,18 @@
 <aside class="category-sidebar">
         <div class="category-header">
-            <h4 class="category-title"><i class="fas fa-align-left"></i><span>asdas</span></h4><button
+            <h4 class="category-title"><i class="fas fa-align-left"></i><span>@lang('words.category_list')</span></h4><button
                 class="category-close"><i class="icofont-close"></i></button>
         </div>
         <ul class="category-list">
-            <h5 class="mt-2">Mobile navbar</h5>
+            <li class="category-item">
+                @foreach ($categories as $c)
+                    <li class="category-item">
+                        <a class="category-link" href="{{ route('web.category.products.show', $c->slug) }}">
+                            <span>{{ $c->title }}</span>
+                        </a>
+                    </li>
+                @endforeach
+            </li>
         </ul>
         <div class="category-footer">
             <p>All Rights Reserved by <a href="#">canseworks</a></p>
