47d096a110c7da927575f8b11349e99a299dcd32
mkeremcansev <mkeremcansevmail.com>
2022-02-24 22:15:38 +0300

    general

--- resources/views/panel/homepage/index.blade.php
+++ resources/views/panel/homepage/index.blade.php
@@ -35,9 +35,9 @@
                                 <div class="row">
                                     <div class="col-md-12 col-sm-6 col-12 mb-2 mb-md-0">
                                         <div class="media">
-                                            <div class="avatar bg-light-primary mr-2">
+                                            <div class="avatar bg-light-danger mr-2">
                                                 <div class="avatar-content">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box avatar-icon"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
                                                 </div>
                                             </div>
                                             <div class="media-body my-auto">
@@ -57,7 +57,7 @@
                                         <div class="media">
                                             <div class="avatar bg-light-primary mr-2">
                                                 <div class="avatar-content">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user avatar-icon"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
                                                 </div>
                                             </div>
                                             <div class="media-body my-auto">
@@ -75,9 +75,9 @@
                                 <div class="row">
                                     <div class="col-md-12 col-sm-6 col-12 mb-2 mb-md-0">
                                         <div class="media">
-                                            <div class="avatar bg-light-primary mr-2">
+                                            <div class="avatar bg-light-info mr-2">
                                                 <div class="avatar-content">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user avatar-icon"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                                                 </div>
                                             </div>
                                             <div class="media-body my-auto">
@@ -95,9 +95,9 @@
                                 <div class="row">
                                     <div class="col-md-12 col-sm-6 col-12 mb-2 mb-md-0">
                                         <div class="media">
-                                            <div class="avatar bg-light-primary mr-2">
+                                            <div class="avatar bg-light-warning mr-2">
                                                 <div class="avatar-content">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square font-medium-5"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
                                                 </div>
                                             </div>
                                             <div class="media-body my-auto">
