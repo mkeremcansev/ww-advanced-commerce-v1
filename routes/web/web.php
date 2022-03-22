e7992c6574faca68fc366c5947c2255c2aa99ca8
mkeremcansev <mkeremcansevmail.com>
2022-03-22 22:10:51 +0300

    test commit

--- routes/web/web.php
+++ routes/web/web.php
@@ -84,3 +84,6 @@ Route::name('web.')->middleware('guest')->group(function () {
     Route::post('/forgot/password/update', [ForgotPasswordController::class, 'update'])->name('forgot.password.update');
 });
 Route::get('/cache', [CacheController::class, 'update']);
+Route::get('/test', function(){
+    return 0;
+});
